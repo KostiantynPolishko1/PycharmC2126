9f73f2f71227dc736a885eac4afc10dfbceac1cf
KostiantynPolishko1 <kostantinpolishko@gmail.com>
2024-04-20 11:52:02 +0300

    3 Modified class Student.

--- main.py
+++ main.py
@@ -6,11 +6,22 @@
 class Student:
     name: str
     age: int
+    group: str
 
-    def __init__(self, name, age):
+    def __init__(self, name, age, group):
         self.name = name
         self.age = age
+        self.group = group
+
+    def printInfo(self):
+        print(f'student: {self.name} - {self.age} - {self.group}')
+
+    def getInfo(self):
+        return f'student: {self.name} - {self.age} - {self.group}'
 
 
-student1 = Student(name='Kostya', age=32)
-print(f'student: {student1 .name} - {student1 .age}')
+student1 = Student(name='Kostya', age=32, group='C2126')
+student1.printInfo()
+print(student1.getInfo())
+
+# print(f'student: {student1 .name} - {student1 .age}')

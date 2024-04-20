15eedb3768a6ab67f7373205cb4da74b2a5babfd
KostiantynPolishko1 <kostantinpolishko@gmail.com>
2024-04-20 11:00:16 +0300

    2 Created class Student.

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print('Hello teams C2126')
+# xValue = 10
+# xWeight = 5.235
+# xName = 'PyCharm'
+# flag = True
+
+class Student:
+    name: str
+    age: int
+
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+
+student1 = Student(name='Kostya', age=32)
+print(f'student: {student1 .name} - {student1 .age}')

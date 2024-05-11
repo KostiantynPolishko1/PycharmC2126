892355e5134b916d27de014376c89768180b3b00
KostiantynPolishko1 <kostantinpolishko@gmail.com>
2024-05-11 10:20:54 +0300

    3 description closure method in class

---
+++ main7_1.py
@@ -0,0 +1,15 @@
+class Calculation:
+    typeOperation: str
+
+    def __init__(self, typeOperation: str):
+        self.typeOperation = typeOperation
+
+    def __call__(self, a: float, b: float)-> float:
+        if self.typeOperation == '+':
+            return a + b
+        else:
+            return f'operation \"{self.typeOperation}\" has not realization'
+
+sumNumbers = Calculation(typeOperation='-')
+result = sumNumbers(a=5, b=2)
+print(result)

9c873651abae347dcf426b41a99c5fefe38c6675
KostiantynPolishko1 <kostantinpolishko@gmail.com>
2024-05-11 10:42:25 +0300

    4 description __str__ method in class

--- main7_1.py
+++ main7_1.py
@@ -4,12 +4,16 @@ class Calculation:
     def __init__(self, typeOperation: str):
         self.typeOperation = typeOperation
 
+    def __str__(self):
+        return f'operation \"{self.typeOperation}\"'
+
     def __call__(self, a: float, b: float)-> float:
         if self.typeOperation == '+':
             return a + b
         else:
             return f'operation \"{self.typeOperation}\" has not realization'
 
-sumNumbers = Calculation(typeOperation='-')
+sumNumbers = Calculation(typeOperation='+')
 result = sumNumbers(a=5, b=2)
 print(result)
+print(sumNumbers)
